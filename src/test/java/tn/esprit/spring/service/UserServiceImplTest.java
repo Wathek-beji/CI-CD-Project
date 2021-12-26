d8dfa1e6aa3c54797f701d7a499bd61e233b7b5d
wathek beji <bejiwathek@gmail.com>
2021-12-26 02:30:52 +0100

    added docker registry role

--- src/test/java/tn/esprit/spring/service/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/UserServiceImplTest.java
@@ -59,8 +59,8 @@ public class UserServiceImplTest {
 		
 		@Test
 		public void testDeleteUser() {
-			us.deleteUser("20");
-			Assert.assertNull(us.retrieveUser("20"));
+			us.deleteUser("21");
+			Assert.assertNull(us.retrieveUser("21"));
 
 		}
 		
